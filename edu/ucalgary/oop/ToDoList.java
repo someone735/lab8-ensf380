6e25988376ecf57b74c47d80dc252e725749ced9
Some Person <smeone83@gmail.com>
2024-02-29 15:08:40 -0700

    Update ToDoList.java

--- edu/ucalgary/oop/ToDoList.java
+++ edu/ucalgary/oop/ToDoList.java
@@ -1,8 +1,15 @@
 package edu.ucalgary.oop;
 
-import java.util.List;
+import java.util.*;
 
-public class ToDoList {
+public class ToDoList implements IToDoList{
+    private List<Task> tasks;
+    private Stack<List<Task>> history;
+
+    public ToDoList(){
+        this.tasks = new List<Task>();
+        this.history = new Stack<List<Task>>();
+    }
 
     @Override
     public List<Task> listTasks() {
@@ -32,4 +39,5 @@ public class ToDoList {
 
         }
     }
+
 }
