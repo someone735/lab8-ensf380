dde8b6cfe36b3361d12ec8024a464f054cd148b5
Bella <144405431+bellaajang@users.noreply.github.com>
2024-02-29 15:07:03 -0700

    .

--- edu/ucalgary/oop/ToDoList.java
+++ edu/ucalgary/oop/ToDoList.java
@@ -1,5 +1,35 @@
 package edu.ucalgary.oop;
 
+import java.util.List;
+
 public class ToDoList {
-    
+
+    @Override
+    public List<Task> listTasks() {
+        return tasks;
+    }
+
+    @Override
+    public void undo() {
+        if (!history.isEmpty()) {
+            tasks = history.pop();
+        }
+    }
+
+    @Override
+    public void deleteTask(String id) {
+
+        saveState();
+
+        Iterator<Task> iterator = tasks.iterator();
+        if (iterator.hasNext()) {
+            Task task = iterator.next();
+
+            if (task.getId().equals(id)) {
+                iterator.remove();
+                break;
+            }
+
+        }
+    }
 }
