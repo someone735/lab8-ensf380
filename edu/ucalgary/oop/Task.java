8fead6a3c092097f824f8a8544d94101c4d09194
Some Person <smeone83@gmail.com>
2024-02-29 14:30:02 -0700

    Update Task.java

--- edu/ucalgary/oop/Task.java
+++ edu/ucalgary/oop/Task.java
@@ -3,7 +3,26 @@ package edu.ucalgary.oop;
 import java.util.Objects;
 
 public class Task {
-
+    private String id;
+    private String title;
+    private boolean isCompleted;
+    public Task(String id, String title){
+        this.id = id;
+        this.title = title;
+        this.isCompleted = false;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public void setCompleted(boolean isCompleted) {
+        this.isCompleted = isCompleted;
+    }
+    public String getId() {
+        return id;
+    }
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
